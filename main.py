306fe53a18c566f21dbc3c9f264fb2a97988c918
Mike Fabre <mijofa+github@fabre.id.au>
2014-03-20 20:28:35 +1100

    Added multi-camera, mirror display, removed some debugging.
    * Now has 2 cameras configured, should be extremely easy to add more
    now, but I shouldn't need to
    * Mirrors the live camera preview, by flipping veritcally then rotating.
    I'm pretty sure this is done badly.

--- main.py
+++ main.py
@@ -2,24 +2,36 @@ import kivy
 kivy.require('1.6.0') # This is the version available in the Debian Wheezy apt repo, I would prefer to remain compatible with that.
 
 from kivy.app import App
-#from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.camera import Camera
+from kivy.uix.scatter import Scatter
 from kivy.uix.gridlayout import GridLayout
 
-cams = [Camera(index=0, resolution=(640,480), size=(640,480))] # May add more than one
+cameras = [Camera(index=0, resolution=(1280,960), size=(640,480), play=True), Camera(index=1, resolution=(640,480), size=(640,480), play=True)] # May add more than one.
 
 class Main(App):
-    def capture(self, *args):
-        print(cams[0]._camera.texture)
-        print(cams[0]._camera.texture.save('/tmp/capture.png', flipped=False))
+    def capture(self, btn):
+        # Capture an image.
+        filename = "/tmp/capture_%d.png" % btn.cam_index
+        print(filename)
+        return cameras[btn.cam_index].texture.save(filename, flipped=False)
+    def flip_on_tex_load(self, cam, *args):
+        cam.texture.flip_vertical()
     def build(self):
-        root = GridLayout()
-        cams[0].play = True
-        root.add_widget(cams[0])
-        btn = Button(text="Capture")
-        btn.bind(on_press=self.capture)
-        root.add_widget(btn)
+        root = GridLayout(cols=2, rows=2)
+        for cam_index in range(0,len(cameras)):
+            cam = cameras[cam_index]
+            if cam != None:
+                if True: ## FIXME: This seems horrible and yucky to me. Why can't I just flip horizontally? And why can't I easily just rotate the camera widget?
+                    sctr = Scatter(do_rotation=False, do_scale=False, do_translation_y=False, do_translation_x=False)
+                    cam.bind(texture=self.flip_on_tex_load)
+                    sctr.rotation = 180
+                    sctr.add_widget(cam)
+                    root.add_widget(sctr)
+                btn = Button(text="Capture")
+                btn.cam_index = cam_index
+                btn.bind(on_press=self.capture)
+                root.add_widget(btn)
         return root
 
 Main().run()
