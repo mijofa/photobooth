106405c6b66be6ca14dbb64f3d6a4d98ea092a45
Mike Fabre <mikef@cyber.com.au>
2014-03-21 14:47:20 +1100

    Change button size. Add a few comments after battling with rotation and giving up.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import kivy
 kivy.require('1.6.0') # This is the version available in the Debian Wheezy apt repo, I would prefer to remain compatible with that.
+### So with Kivy 1.6.0 on my netbook the scatter().rotation works, but the Camera().texture.flip_vertical() does no. Is this the Kivy version or the camera?
 
 from kivy.app import App
 from kivy.uix.button import Button
@@ -18,7 +19,7 @@ class Main(App):
     def flip_on_tex_load(self, cam, *args):
         cam.texture.flip_vertical()
     def build(self):
-        root = GridLayout(cols=2, rows=2)
+        root = GridLayout(rows=2) # FIXME/INVESTIGATE: I'd like to be able to add items top-left, bottom-left, top-right, bottom-right. May need to change the order I'm adding things.
         for cam_index in range(0,len(cameras)):
             cam = cameras[cam_index]
             if cam != None:
@@ -31,6 +32,7 @@ class Main(App):
                 btn = Button(text="Capture")
                 btn.cam_index = cam_index
                 btn.bind(on_press=self.capture)
+                btn.size_hint = [0.1, 0.1]
                 root.add_widget(btn)
         return root
 
