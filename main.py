798a65c60466b485f315a06a1a60e16fe12aa909
Mike Fabre <mikef@cyber.com.au>
2014-04-08 13:25:37 +1000

    Only display one camera, flash screen when saving.
    I removed the GridLayout, in favour displaying one camera with no
    button.
    I moved the capture function to inside the Camera on_touch_down.
    I made the camera go full-white for 0.1 seconds immediately after
    saving.

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ kivy.require('1.6.0') # This is the version available in the Debian Wheezy apt r
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.clock import Clock
 from kivy.uix.button import Button
 from kivy.uix.camera import Camera
 from kivy.uix.scatter import Scatter
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 
 import time
 
@@ -36,29 +38,26 @@ class MirrorCamera(Camera):
         self.texture_size = list(self.texture.size)
         self.texture.flip_vertical()
 ###
+    def on_touch_down(self, *args, **kwargs):
+        super(Camera, self).on_touch_down(*args, **kwargs)
+        # Capture an image.
+        filename = "/tmp/capture-%d_%f.png" % (self.index, time.time())
+        print(filename)
+        if self.texture != None: # Camera not connected?
+            try: print(self.texture.save(filename, flipped=False))
+            except AttributeError: pass # Might be an older version of Kivy
+        self.color = [5,5,5,1]
+        Clock.schedule_once(self.flash_reset, 0.1)
+    def flash_reset(self, *args, **kwargs):
+        self.color = [1,1,1,1]
 
 cameras = [MirrorCamera(index=0, resolution=(1280,960), size=(640,480), play=True), MirrorCamera(index=1, resolution=(1280,960), size=(640,480), play=True)] # Capture the highest possible resolution (current v4l + USB2 can't handle more than 720p) but only display low res. This give me highest possible image captures with a standard size widget regardless what cameras are used.
 
 class Main(App):
-    def capture(self, btn):
-        # Capture an image.
-        filename = "/tmp/capture-%d_%f.png" % (btn.cam_index, time.time())
-        print(filename)
-        return cameras[btn.cam_index].texture.save(filename, flipped=False)
     def build(self):
-        root = GridLayout(rows=1) # FIXME/INVESTIGATE: I'd like to be able to add items top-left, bottom-left, top-right, bottom-right. May need to change the order I'm adding things.
-        root.spacing = 10
-        for cam_index in range(0,len(cameras)):
-            cam = cameras[cam_index]
-            if cam != None:
-                col = GridLayout(rows=2) # I want to create a column at a time, this seems easiest.
-                col.add_widget(cam)
-                btn = Button(text="Capture")
-                btn.cam_index = cam_index
-                btn.bind(on_press=self.capture)
-                btn.size_hint = [0.1, 0.1]
-                col.add_widget(btn)
-                root.add_widget(col)
+        root = FloatLayout()
+        root.add_widget(cameras[0])
+        cameras[0].pos_hint['center'] = [0.5,0.5]
         return root
 
 Main().run()
